361a51dcab109c0bbf3c507cc56e0d69e69d3485
Keyaan K. <91108855+keyaan-khan@users.noreply.github.com>
2025-03-01 09:10:34 -0500

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -27,8 +27,6 @@ var config_data = `
         "type": "level",
         "choices": {
           "qm": "Quals<br>",
-          "sf": "Semifinals<br>",
-          "f": "Finals"
         },
         "defaultValue": "qm",
         "required": "true"
